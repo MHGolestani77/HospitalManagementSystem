fe1cb8739da04bbba743bce01ee5c02354792319
AliBeikdolati <ali.beik2lati@gmail.com>
2018-12-01 11:59:21 +0330

    update 1

--- page/admin/insert.php
+++ page/admin/insert.php
@@ -1,53 +1,132 @@
 <?php include "header.php"; ?>
 
 	<div id="main">
-		<dic class="container row">
-			<div class="col-lg-5 col-md-4">
-				<ul>
-					<li>
-						<p>افزودن پزشک</p>
-					</li>
-					<li>
-						<p>افزودن پزشک</p>
-					</li>
-					<li>
-						<p>افزودن بیمار</p>
-					</li>
-				</ul>
+		<div class="container" id="insert">
+			<div class="row">
+				<div class="col-lg-4 col-md-4" id="right">
+					<ul class="insert">
+						<a href="#">
+							<li>
+								<p>افزودن پزشک</p>
+							</li>
+						</a> 
+						<a href="#">
+							<li>
+								<p>افزودن پذیرش</p>
+							</li>
+						</a> 
+						<a href="#">
+							<li>
+								<p>افزودن بیمار</p>
+							</li>
+						</a> 
+					</ul>
+				</div>
+
+				<!-- add doctor -->
+				<div class="col-lg-7 col-md-8" id="add-doctor">
+					<form class="form-horizontal">
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="fname">نام</label>
+							<input type="text" name="first-name" class="form-control col-lg-8" id="fname">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="lname">نام خانوادگی</label>
+							<input type="text" name="last-name" class="form-control col-lg-8" id="lname">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="fa-name">نام پدر</label>
+							<input type="text" name="father-name" class="form-control col-lg-8" id="fa-name">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="ncode">کد ملی</label>
+							<input type="text" name="nationalcode" class="form-control col-lg-8" id="ncode">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="scode">شماره شناسنامه</label>
+							<input type="text" name="id-number" class="form-control col-lg-8" id="scode">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="mnumber">شماره همراه</label>
+							<input type="text" name="mobile-no" class="form-control col-lg-8" id="mnumber">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="expert">تخصص</label>
+							<input type="text" name="expert" class="form-control col-lg-8" id="expert">
+						</div>
+					</form>
+				</div>
+
+			<!-- end of adding doctor -->
+
+
+			<!-- add clerk -->
+				<div class="col-lg-7 col-md-8" id="add-clerk">
+					<form class="form-horizontal">
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="fname">نام</label>
+							<input type="text" name="first-name" class="form-control col-lg-8" id="fname">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="lname">نام خانوادگی</label>
+							<input type="text" name="last-name" class="form-control col-lg-8" id="lname">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="fa-name">نام پدر</label>
+							<input type="text" name="father-name" class="form-control col-lg-8" id="fa-name">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="ncode">کد ملی</label>
+							<input type="text" name="nationalcode" class="form-control col-lg-8" id="ncode">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="scode">شماره شناسنامه</label>
+							<input type="text" name="id-number" class="form-control col-lg-8" id="scode">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="mnumber">شماره همراه</label>
+							<input type="text" name="mobile-no" class="form-control col-lg-8" id="mnumber">
+						</div>
+					</form>
+				</div>
+				<!-- end of adding clerk -->
+
+				<!-- add illness -->
+				<div class="col-lg-7 col-md-8" id="add-ill">
+					<form class="form-horizontal">
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="fname">نام</label>
+							<input type="text" name="first-name" class="form-control col-lg-8" id="fname">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="lname">نام خانوادگی</label>
+							<input type="text" name="last-name" class="form-control col-lg-8" id="lname">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="fa-name">نام پدر</label>
+							<input type="text" name="father-name" class="form-control col-lg-8" id="fa-name">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="ncode">کد ملی</label>
+							<input type="text" name="nationalcode" class="form-control col-lg-8" id="ncode">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="scode">شماره شناسنامه</label>
+							<input type="text" name="id-number" class="form-control col-lg-8" id="scode">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="mnumber">شماره همراه</label>
+							<input type="text" name="mobile-no" class="form-control col-lg-8" id="mnumber">
+						</div>
+						<div class="form-group row">
+							<label class="control-label col-sm-4 col-lg-3" for="expert">تخصص</label>
+							<input type="text" name="expert" class="form-control col-lg-8" id="expert">
+						</div>
+					</form>
+				</div>
+				<!-- end of adding illness -->
 			</div>
-			<div class="col-lg-7 col-md-8">
-				<form class="form-horizontal">
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="fname">نام</label>
-						<input type="text" name="first-name" class="form-control col-lg-8" id="fname">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="lname">نام خانوادگی</label>
-						<input type="text" name="last-name" class="form-control col-lg-8" id="lname">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="fa-name">نام پدر</label>
-						<input type="text" name="father-name" class="form-control col-lg-8" id="fa-name">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="ncode">کد ملی</label>
-						<input type="text" name="nationalcode" class="form-control col-lg-8" id="ncode">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="scode">شماره شناسنامه</label>
-						<input type="text" name="id-number" class="form-control col-lg-8" id="scode">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="mnumber">شماره همراه</label>
-						<input type="text" name="mobile-no" class="form-control col-lg-8" id="mnumber">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="expert">تخصص</label>
-						<input type="text" name="expert" class="form-control col-lg-8" id="expert">
-					</div>
-				</form>
-			</div>
-		</dic>
+		</div>
 	</div>
 
 <?php include "footer.php"; ?>
--- page/clerk/insert.php
+++ page/clerk/insert.php
@@ -1,49 +1,5 @@
 <?php include "header.php"; ?>
 
-	<div id="main">
-		<dic class="container row">
-			<div class="col-lg-5 col-md-4">
-				<ul>
-					<li>
-						<p>افزودن پزشک</p>
-					</li>
-					<li>
-						<p>افزودن پزشک</p>
-					</li>
-					<li>
-						<p>افزودن بیمار</p>
-					</li>
-				</ul>
-			</div>
-			<div class="col-lg-7 col-md-8">
-				<form class="form-horizontal">
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="fname">نام</label>
-						<input type="text" name="first-name" class="form-control col-lg-8" id="fname">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="lname">نام خانوادگی</label>
-						<input type="text" name="last-name" class="form-control col-lg-8" id="lname">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="fa-name">نام پدر</label>
-						<input type="text" name="father-name" class="form-control col-lg-8" id="fa-name">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="ncode">کد ملی</label>
-						<input type="text" name="nationalcode" class="form-control col-lg-8" id="ncode">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="scode">شماره شناسنامه</label>
-						<input type="text" name="id-number" class="form-control col-lg-8" id="scode">
-					</div>
-					<div class="form-group row">
-						<label class="control-label col-sm-4 col-lg-3" for="mnumber">شماره همراه</label>
-						<input type="text" name="mobile-no" class="form-control col-lg-8" id="mnumber">
-					</div>
-				</form>
-			</div>
-		</dic>
-	</div>
+	
 
 <?php include "footer.php"; ?>
