f9a82a03f5e13dc2716bdf0aa243218702dd26fa
AliBeikdolati <ali.beik2lati@gmail.com>
2018-12-01 09:38:54 +0330

    fase2

---
+++ page/admin/header.php
@@ -0,0 +1,79 @@
+<?php include "../../functions.php"; ?>
+
+<?php $ref = rand()*rand(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SUT</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="http://hms.com/assets/css/styles.css?<?php echo $ref; ?>">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+</head>
+<body>
+	<div id="header">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  	<a class="navbar-brand" href="#">
+				    <img src="assets/img/logo.png" width="40" height="40" class="" alt="">
+		  		بسیج دانشجویی</a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    	<span class="navbar-toggler-icon"></span>
+		  	</button>
+
+		  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    	<ul class="navbar-nav ml-auto rtl">
+		      		<li class="nav-item text-right active">
+		        		<a class="nav-link" href="#">صفحه نخست<span class="sr-only">(current)</span></a>
+		      		</li>
+
+		      		<!-- <li class="nav-item text-right">
+		        		<a class="nav-link" href="#"></a>
+		      		</li> -->
+
+		      		<li class="nav-item dropdown">
+		        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		        			مدیریت پزشک
+		        		</a>
+		        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          				<a class="dropdown-item" href="#">افزودن پزشک</a>
+		          				<a class="dropdown-item" href="#">لیست پزشک</a>
+		        			</div>
+		      		</li>
+		      	
+		      		<li class="nav-item dropdown">
+		        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		        			مدیریت پذیرش
+		        		</a>
+		        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          				<a class="dropdown-item" href="#">افزودن پذیرش</a>
+		          				<a class="dropdown-item" href="#">لیست پذیرش</a>
+		        			</div>
+		      		</li>
+		      	
+		      		<li class="nav-item dropdown">
+		        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		        			مدیریت بیمار
+		        		</a>
+		        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          				<a class="dropdown-item" href="#">افزودن بیمار</a>
+		          				<a class="dropdown-item" href="#">لیست بیمار</a>
+		        			</div>
+		      		</li>
+		    	</ul>
+		    	<form class="form-inline my-2 my-lg-0">
+		    		<div class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			        		مدیر بیمارستان
+			        	</a>
+			        	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          		<a class="dropdown-item" href="account.php">اطلاعات کاربری</a>
+			          		<a class="dropdown-item" href="#">تنظیمات</a>
+			          		<div class="dropdown-divider"></div>
+			          		<a class="dropdown-item" href="#">خروج</a>
+			        	</div>
+		    		</div>
+		      		<img src="assets/img/memberId.jpg" alt="" class="my-2 my-sm-0">
+		    	</form>
+		  	</div>
+		</nav>
+	</div>
--- page/admin/insert.php
+++ page/admin/insert.php
@@ -1,7 +1,7 @@
 <?php include "header.php"; ?>
 
 	<div id="main">
-		<dic class="container">
+		<dic class="container row">
 			<div class="col-lg-5 col-md-4">
 				<ul>
 					<li>
@@ -17,32 +17,32 @@
 			</div>
 			<div class="col-lg-7 col-md-8">
 				<form class="form-horizontal">
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="fname">نام</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="fname">نام</label>
 						<input type="text" name="first-name" class="form-control col-lg-8" id="fname">
 					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="lname">نام خانوادگی</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="lname">نام خانوادگی</label>
 						<input type="text" name="last-name" class="form-control col-lg-8" id="lname">
 					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="fa-name">نام پدر</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="fa-name">نام پدر</label>
 						<input type="text" name="father-name" class="form-control col-lg-8" id="fa-name">
 					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="ncode">کد ملی</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="ncode">کد ملی</label>
 						<input type="text" name="nationalcode" class="form-control col-lg-8" id="ncode">
 					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="scode">شماره شناسنامه</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="scode">شماره شناسنامه</label>
 						<input type="text" name="id-number" class="form-control col-lg-8" id="scode">
 					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="mnumber">شماره همراه</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="mnumber">شماره همراه</label>
 						<input type="text" name="mobile-no" class="form-control col-lg-8" id="mnumber">
 					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2 col-lg-2" for="expert">تخصص</label>
+					<div class="form-group row">
+						<label class="control-label col-sm-4 col-lg-3" for="expert">تخصص</label>
 						<input type="text" name="expert" class="form-control col-lg-8" id="expert">
 					</div>
 				</form>
